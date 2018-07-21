c7e152ec68d450609373253d84acd1ae059b3844
Daisuke Baba <baba.daisuke@gmail.com>
2018-07-21 10:19:36 +0900

    Fix typo

--- src/smartmesh.js
+++ src/smartmesh.js
@@ -87,7 +87,7 @@ export default function(RED) {
         delete smartMeshClients[this.serialport];
         this.client.shutdown().then(() => {
           done();
-        }).cath((err) => {
+        }).catch((err) => {
           this.log(err);
           done();
         });
