33d2951e1b0c36380abfc1b3c76a4d92d4d59832
Daisuke Baba <baba.daisuke@gmail.com>
2019-03-07 20:58:36 +0900

    Refactor ProtocolClientProxy so to isolate OAP specific functions from protocol-independent ones

--- deps/python/protocol_client_proxy.py
+++ deps/python/protocol_client_proxy.py
@@ -34,12 +34,6 @@ from SmartMeshSDK.protocols.oap \
 
 DEFAULT_SERIALPORT = '/dev/ttyUSB3'
 SYSLOG_ADDRESS = '/var/run/syslog' if sys.platform == "darwin" else '/dev/log'
-TLV_FORMATS = {
-    'byte': 'TLVByte',
-    'short': 'TLVShort',
-    'long': 'TLVLong',
-    'string': 'TLVString'
-}
 
 
 class NullHandler(logging.Handler):
@@ -56,7 +50,166 @@ handler.setFormatter(formatter)
 AppUtils.configureLogging()
 
 
-class OAPClientProxy(object):
+class ProtocolUtils(object):
+    @staticmethod
+    def format_mac_address(mac):
+        mac_address = None
+        if isinstance(mac, list):
+            mac_address = '-'.join([('%0.2x' % x) for x in mac])
+        elif isinstance(mac, basestring):
+            if ':' in mac:
+                mac_address = mac.replace(':', '-').lower()
+            else:
+                mac_address = mac.lower()
+        if not mac_address:
+            raise ValueError('Unsupported mac address value [%s]' % str(mac))
+        return mac_address
+
+    @staticmethod
+    def to_byte_array_mac(mac):
+        mac_address = None
+        if isinstance(mac, list):
+            mac_address = mac
+        elif isinstance(mac, basestring):
+            if '-' in mac:
+                mac_address = [
+                    int(d, 16) for d in
+                    filter(lambda p: p.strip() != '', mac.split('-'))
+                ]
+            elif ':' in mac:
+                mac_address = [
+                    int(d, 16) for d in
+                    filter(lambda p: p.strip() != '', mac.split(':'))
+                ]
+        if not mac_address:
+            raise ValueError('Unsupported mac address value [%s]' % str(mac))
+        return mac_address
+
+    @staticmethod
+    def resolve_protocol_type(message):
+        return 'oap'
+
+
+class OAPSupport(object):
+    TLV_FORMATS = {
+        'byte': 'TLVByte',
+        'short': 'TLVShort',
+        'long': 'TLVLong',
+        'string': 'TLVString'
+    }
+
+    def __init__(self, subscriber,
+                 send_data_func,
+                 send_message_func,
+                 emit_event_func):
+        self.send_data_func = send_data_func
+        self.send_message_func = send_message_func
+        self.emit_event_func = emit_event_func
+        self.oap_dispatch = OAPDispatcher.OAPDispatcher()
+        self.oap_dispatch.register_notif_handler(self.handle_oap_data)
+        subscriber.subscribe(
+            notifTypes=[
+               IpMgrSubscribe.IpMgrSubscribe.NOTIFDATA,
+            ],
+            fun=self.oap_dispatch.dispatch_pkt,
+            isRlbl=False,
+        )
+        self.oap_clients = {}
+
+    def send(self, mac, request_id, message):
+        def oap_callback(mac_address, oap_resp):
+            self.emit_event_func({
+                'event': 'result',
+                'id': request_id,
+                'mac': ProtocolUtils.format_mac_address(mac_address),
+                'command': message['command'],
+                'result': oap_resp['result']
+            })
+
+        if mac not in self.oap_clients:
+            self.oap_clients[mac] = OAPClient.OAPClient(
+                ProtocolUtils.to_byte_array_mac(mac),
+                self.send_data_func,
+                self.oap_dispatch
+            )
+
+        # send packet
+        self.oap_clients[mac].send(
+            # command
+            self.to_oap_command(message),
+            # address
+            self.to_oap_address(message),
+            # parameters
+            data_tags=self.to_oap_tags(message),
+            # callback
+            cb=oap_callback
+        )
+
+    def handle_oap_data(self, mac, notif):
+        """Called when the OAP dispatcher can succesfully parse
+           received data as OAP
+        """
+        mac_address = ProtocolUtils.format_mac_address(mac)
+        try:
+            message = notif._asdict()
+            message['event'] = 'data'
+            message['mac'] = mac_address
+            self.send_message_func(message)
+        except Exception as err:
+            self.send_message_func({
+                'event': 'error',
+                'mac': mac_address,
+                'message':
+                    'failed to handle OAP data, error ({0})\n{1}'
+                    .format(
+                        type(err),
+                        err
+                    )
+            })
+            sys.exit(99)
+
+    def to_oap_command(self, message):
+        m = message['command'].upper() if 'command' in message else 'GET'
+        val = OAPMessage.CmdType.__dict__[m] \
+            if m in OAPMessage.CmdType.__dict__ \
+            else None
+        if val is None:
+            raise ValueError('Command:[%s] is unknown' % m)
+        else:
+            return val
+
+    def to_oap_address(self, message):
+        a = message['address'].upper() if 'address' in message else None
+        if not a:
+            raise ValueError('`address` is missing')
+        if isinstance(a, list):
+            return a
+        elif isinstance(a, basestring):
+            return [
+                int(d) for d in
+                filter(lambda p: p.strip() != '', a.split('/'))
+            ]
+        else:
+            raise ValueError('`address` value[%s] is invalid' % str(a))
+
+    def to_oap_tags(self, message):
+        tags = message['tags'] if 'tags' in message else []
+        return [
+            self.to_oap_tlv(tag) for tag in tags
+        ]
+
+    def to_oap_tlv(self, tag):
+        format = tag['format'].lower() if 'format' in tag else 'byte'
+        if format not in OAPSupport.TLV_FORMATS:
+            raise ValueError('`format` value[%s] is invalid' % str(format))
+        value = tag['value']
+        if format != 'string' and isinstance(value, basestring):
+            value = int(value, 16)
+        return OAPMessage.__dict__[OAPSupport.TLV_FORMATS[format]](
+            t=int(tag['tag']), v=value)
+
+
+class ProtocolClientProxy(object):
     def __init__(self, serialport=DEFAULT_SERIALPORT):
         # connect to manager
         self.connector = IpMgrConnectorSerial.IpMgrConnectorSerial()
@@ -77,19 +230,8 @@ class OAPClientProxy(object):
             })
             sys.exit(1)
 
-        self.protocol_clients = {}
-        self.oap_dispatch = OAPDispatcher.OAPDispatcher()
-        self.oap_dispatch.register_notif_handler(self.handle_oap_data)
-
         self.subscriber = IpMgrSubscribe.IpMgrSubscribe(self.connector)
         self.subscriber.start()
-        self.subscriber.subscribe(
-            notifTypes=[
-               IpMgrSubscribe.IpMgrSubscribe.NOTIFDATA,
-            ],
-            fun=self.oap_dispatch.dispatch_pkt,
-            isRlbl=False,
-        )
         self.subscriber.subscribe(
             notifTypes=[
                 IpMgrSubscribe.IpMgrSubscribe.NOTIFEVENT,
@@ -106,6 +248,13 @@ class OAPClientProxy(object):
             fun=self.on_disconnected,
             isRlbl=True,
         )
+
+        self.supported_protocols = {
+            'oap': OAPSupport(self.subscriber,
+                              self.connector.dn_sendData,
+                              self.send_message,
+                              self.emit_event)
+        }
 
     def send_message(self, message):
         print(json.dumps(message))
@@ -168,36 +317,11 @@ class OAPClientProxy(object):
 
         mac = ''
         try:
-            mac = self.format_mac_address(message['mac'])
+            mac = ProtocolUtils.format_mac_address(message['mac'])
             request_id = message['id'] if 'id' in message else None
-
-            def oap_callback(mac_address, oap_resp):
-                self.emit_event({
-                    'event': 'result',
-                    'id': request_id,
-                    'mac': self.format_mac_address(mac_address),
-                    'command': message['command'],
-                    'result': oap_resp['result']
-                })
-
-            if mac not in self.protocol_clients:
-                self.protocol_clients[mac] = OAPClient.OAPClient(
-                    self.to_byte_array_mac(mac),
-                    self.connector.dn_sendData,
-                    self.oap_dispatch
-                )
-
-            # send packet
-            self.protocol_clients[mac].send(
-                # command
-                self.to_oap_command(message),
-                # address
-                self.to_oap_address(message),
-                # parameters
-                data_tags=self.to_oap_tags(message),
-                # callback
-                cb=oap_callback
-            )
+            self.supported_protocols[
+                ProtocolUtils.resolve_protocol_type(message)
+            ].send(mac, request_id, message)
         except Exception as err:
             self.send_message({
                 'event': 'error',
@@ -232,7 +356,8 @@ class OAPClientProxy(object):
             'type': notifName
         }
         try:
-            message['mac'] = self.format_mac_address(notifParams.macAddress)
+            message['mac'] = ProtocolUtils.format_mac_address(
+                             notifParams.macAddress)
         except Exception:
             pass
         self.emit_event(message)
@@ -248,101 +373,6 @@ class OAPClientProxy(object):
         # exit with error code 16=>disconnected
         sys.exit(16)
 
-    def handle_oap_data(self, mac, notif):
-        """Called when the OAP dispatcher can succesfully parse
-           received data as OAP
-        """
-        mac_address = self.format_mac_address(mac)
-        try:
-            message = notif._asdict()
-            message['event'] = 'data'
-            message['mac'] = mac_address
-            self.send_message(message)
-        except Exception as err:
-            self.send_message({
-                'event': 'error',
-                'mac': mac_address,
-                'message':
-                    'failed to handle OAP data, error ({0})\n{1}'
-                    .format(
-                        type(err),
-                        err
-                    )
-            })
-            sys.exit(99)
-
-    def to_oap_command(self, message):
-        m = message['command'].upper() if 'command' in message else 'GET'
-        val = OAPMessage.CmdType.__dict__[m] \
-            if m in OAPMessage.CmdType.__dict__ \
-            else None
-        if val is None:
-            raise ValueError('Command:[%s] is unknown' % m)
-        else:
-            return val
-
-    def to_oap_address(self, message):
-        a = message['address'].upper() if 'address' in message else None
-        if not a:
-            raise ValueError('`address` is missing')
-        if isinstance(a, list):
-            return a
-        elif isinstance(a, basestring):
-            return [
-                int(d) for d in
-                filter(lambda p: p.strip() != '', a.split('/'))
-            ]
-        else:
-            raise ValueError('`address` value[%s] is invalid' % str(a))
-
-    def to_oap_tags(self, message):
-        tags = message['tags'] if 'tags' in message else []
-        return [
-            self.to_oap_tlv(tag) for tag in tags
-        ]
-
-    def to_oap_tlv(self, tag):
-        format = tag['format'].lower() if 'format' in tag else 'byte'
-        if format not in TLV_FORMATS:
-            raise ValueError('`format` value[%s] is invalid' % str(format))
-        value = tag['value']
-        if format != 'string' and isinstance(value, basestring):
-            value = int(value, 16)
-        return OAPMessage.__dict__[TLV_FORMATS[format]](
-            t=int(tag['tag']), v=value)
-
-    def format_mac_address(self, mac):
-        mac_address = None
-        if isinstance(mac, list):
-            mac_address = '-'.join([('%0.2x' % x) for x in mac])
-        elif isinstance(mac, basestring):
-            if ':' in mac:
-                mac_address = mac.replace(':', '-').lower()
-            else:
-                mac_address = mac.lower()
-        if not mac_address:
-            raise ValueError('Unsupported mac address value [%s]' % str(mac))
-        return mac_address
-
-    def to_byte_array_mac(self, mac):
-        mac_address = None
-        if isinstance(mac, list):
-            mac_address = mac
-        elif isinstance(mac, basestring):
-            if '-' in mac:
-                mac_address = [
-                    int(d, 16) for d in
-                    filter(lambda p: p.strip() != '', mac.split('-'))
-                ]
-            elif ':' in mac:
-                mac_address = [
-                    int(d, 16) for d in
-                    filter(lambda p: p.strip() != '', mac.split(':'))
-                ]
-        if not mac_address:
-            raise ValueError('Unsupported mac address value [%s]' % str(mac))
-        return mac_address
-
 
 if __name__ == "__main__":
     # ask user for serial port number
@@ -357,4 +387,4 @@ if __name__ == "__main__":
     else:
         serialport = sys.argv[1]
 
-    OAPClientProxy(serialport).start()
+    ProtocolClientProxy(serialport).start()
