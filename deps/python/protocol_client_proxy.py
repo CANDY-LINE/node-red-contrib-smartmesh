1e886c1c082a3ceefaa40bba536da188d2d6c867
Daisuke Baba <baba.daisuke@gmail.com>
2019-03-11 10:13:08 +0900

    Update copyright year

--- deps/python/protocol_client_proxy.py
+++ deps/python/protocol_client_proxy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2018 CANDY LINE INC.
+# Copyright (c) 2019 CANDY LINE INC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
